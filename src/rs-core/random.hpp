9b2546d0ee5b70f787c5e0806409ff48141847bf
Ross Smith <rosssmith@me.com>
2025-07-29 14:18:11 +1000

    Use native uint128 where possible

--- src/rs-core/random.hpp
+++ src/rs-core/random.hpp
@@ -20,61 +20,76 @@ namespace RS {
 
     namespace Detail {
 
-        struct Uint128 {
+        #ifdef __GNUC__
 
-            std::uint64_t high {};
-            std::uint64_t low {};
+            using Uint128 = __uint128_t;
 
-            constexpr Uint128() = default;
-            constexpr Uint128(std::uint64_t lo) noexcept: low{lo} {}
-            constexpr Uint128(std::uint64_t hi, std::uint64_t lo) noexcept: high{hi}, low{lo} {}
+        #else
 
-            constexpr Uint128 operator<<(int y) const noexcept {
-                if (y >= 64) {
-                    return {low << (y - 64), 0};
-                } else if (y >= 1) {
-                    return {(high << y) + (low >> (64 - y)), low << y};
-                } else {
-                    return *this;
-                }
-            }
+            class Uint128 {
 
-            constexpr Uint128 operator>>(int y) const noexcept {
-                if (y >= 64) {
-                    return high >> (y - 64);
-                } else if (y >= 1) {
-                    return {high >> y, (low >> y) + (high << (64 - y))};
-                } else {
-                    return *this;
-                }
-            }
+            public:
 
-            constexpr Uint128 operator|(Uint128 y) const noexcept {
-                return {high | y.high, low | y.low};
-            }
+                constexpr Uint128() = default;
+                constexpr Uint128(std::uint64_t lo) noexcept: lo_{lo} {}
 
-            constexpr Uint128 operator+(Uint128 y) const noexcept {
-                Uint128 z{high + y.high, low + y.low};
-                if (z.low < low) {
-                    ++z.high;
-                }
-                return z;
-            }
+                constexpr explicit operator std::uint64_t() const noexcept { return lo_; }
 
-            constexpr Uint128 operator*(Uint128 y) const noexcept {
-                static constexpr std::uint64_t max32 = (1ull << 32) - 1;
-                Uint128 z;
-                for (auto a = 0; a < 128; a += 32) {
-                    auto c = (*this >> a).low & max32;
-                    for (auto b = 0; b < 128; b += 32) {
-                        auto d = (y >> b).low & max32;
-                        z = z + (Uint128{c * d} << (a + b));
+                constexpr Uint128 operator<<(int y) const noexcept {
+                    if (y >= 64) {
+                        return {lo_ << (y - 64), 0};
+                    } else if (y >= 1) {
+                        return {(hi_ << y) + (lo_ >> (64 - y)), lo_ << y};
+                    } else {
+                        return *this;
                     }
                 }
-                return z;
-            }
 
-        };
+                constexpr Uint128 operator>>(int y) const noexcept {
+                    if (y >= 64) {
+                        return hi_ >> (y - 64);
+                    } else if (y >= 1) {
+                        return {hi_ >> y, (lo_ >> y) + (hi_ << (64 - y))};
+                    } else {
+                        return *this;
+                    }
+                }
+
+                constexpr Uint128 operator|(Uint128 y) const noexcept {
+                    return {hi_ | y.hi_, lo_ | y.lo_};
+                }
+
+                constexpr Uint128 operator+(Uint128 y) const noexcept {
+                    Uint128 z{hi_ + y.hi_, lo_ + y.lo_};
+                    if (z.lo_ < lo_) {
+                        ++z.hi_;
+                    }
+                    return z;
+                }
+
+                constexpr Uint128 operator*(Uint128 y) const noexcept {
+                    static constexpr std::uint64_t max32 = (1ull << 32) - 1;
+                    Uint128 z;
+                    for (auto a = 0; a < 128; a += 32) {
+                        auto c = (*this >> a).lo_ & max32;
+                        for (auto b = 0; b < 128; b += 32) {
+                            auto d = (y >> b).lo_ & max32;
+                            z = z + (Uint128{c * d} << (a + b));
+                        }
+                    }
+                    return z;
+                }
+
+            private:
+
+                std::uint64_t hi_{};
+                std::uint64_t lo_{};
+
+                constexpr Uint128(std::uint64_t hi, std::uint64_t lo) noexcept: hi_{hi}, lo_{lo} {}
+
+            };
+
+        #endif
 
     }
 
@@ -118,8 +133,8 @@ namespace RS {
             static constexpr std::uint64_t k = 0xda94'2042'e4dd'58b5ull;
             auto u = state_;
             state_ = u * k + delta_;
-            auto x = (u >> 64).low;
-            auto y = (u | 1).low;
+            auto x = static_cast<std::uint64_t>(u >> 64);
+            auto y = static_cast<std::uint64_t>(u | 1);
             x ^= x >> 32;
             x *= k;
             x ^= x >> 48;
@@ -128,8 +143,8 @@ namespace RS {
         }
 
         constexpr void Pcg::seed(std::uint64_t s0, std::uint64_t s1, std::uint64_t s2, std::uint64_t s3) noexcept {
-            state_ = {s0, s1};
-            delta_ = {s2, s3};
+            state_ = (Detail::Uint128{s0} << 64) + s1;
+            delta_ = (Detail::Uint128{s2} << 64) + s3;
             delta_ = (delta_ << 1) | 1;
             state_ = state_ + delta_;
             (*this)();
